8048ede1a6c5183596038de9412f6315f56bd88b
Mikhail <turnedbold@gmail.com>
2019-06-01 10:47:45 +0900

    Added Lab15

---
+++ Lab15/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab15
+{
+    public partial class Form1 : Form
+    {
+        int number;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number = Int32.Parse(textBox1.Text);
+            number = number * number;
+            textBox2.Text = System.Convert.ToString(number); ;
+            MessageBox.Show("Calculations completed!");
+        }
+    }
+}
